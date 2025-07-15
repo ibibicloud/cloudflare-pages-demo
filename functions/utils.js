5361dca66c7054c8335d12882bddac4ccb4bc6bc
ibibicloud <ibibicloud@gmail.com>
2025-07-16 02:20:24 +0800

    删除 utils.js

--- functions/utils.js
+++
@@ -1,9 +0,0 @@
-
-export const response = (data, rcode, header, cors = false) {
-	// return new Response(JSON.stringify(data), {
-	// 	status: 200,
-	// 	headers: {
-	// 		'Content-Type': 'application/json',
-	// 	},
-	// };
-}
